0270579491db4430b15e317b34e3082f0155d309
Ian Miell <ian.miell@gmail.com>
2015-03-31 20:42:18 +0100

    run

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker build -t kub .
-docker run -ti -v /var/run/docker.sock:/var/run/docker.sock --privileged kub
+docker build -t kubernetes .
+docker run -d --name kubernetes --net=host -v /var/run/docker.sock:/var/run/docker.sock --privileged kub
